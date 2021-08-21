cfb5ca1a487f81aec420f4a7d83d164397bb728c
Summersby95 <james.summersby@gmail.com>
2021-08-21 11:37:31 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Titanic Data Science Challenge',
+    author='James Summersby',
+    license='MIT',
+)
